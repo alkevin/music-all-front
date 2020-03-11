9e1a35667d27f34b20f92a24aa4feceac89c24c8
AKvin <kevin.alvespinto@gmail.com>
2020-03-11 15:50:41 +0100

    add footer

---
+++ src/app/core/components/footer/footer.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  @Input()
+  yearNow: string;
+
+  @Input()
+  appVersion: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
