abc5d9cec0f3de0265a433ea0ac8ff1563738c66
AKvin <kevin.alvespinto@gmail.com>
2020-09-10 10:09:16 +0200

    implement user state in header

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { AppConstants } from '../../../shared/constants/app.constants';
+import { User } from 'src/app/shared/models/user';
+import { DestroyeUser } from './../../../state/actions/user.actions';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/state/reducers';
 
 @Component({
   selector: 'app-header',
@@ -15,14 +19,21 @@ export class HeaderComponent {
   @Input()
   appTitle: string;
 
+  @Input()
+  user: User;
+
   myUrlPhoto: string;
 
-  constructor() {
+  constructor(private store: Store<AppState>) {
     this.myUrlPhoto = 'https://t1.ea.ltmcdn.com/fr/images/1/7/7/les_animaux_les_plus_rares_du_monde_top_18_avec_photos_771_600.jpg';
    }
 
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
+  }
+
+  logout() {
+    this.store.dispatch(DestroyeUser());
   }
 
 }
