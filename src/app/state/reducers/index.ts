531ca142e944b9ce9bf0896200eb7b1257e775fa
AKvin <kevin.alvespinto@gmail.com>
2020-09-10 10:23:09 +0200

    add reducers for store

--- src/app/state/reducers/index.ts
+++ src/app/state/reducers/index.ts
@@ -1,20 +1,17 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
-} from '@ngrx/store';
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import * as fromUserLogged from './user.reducer';
 import { environment } from '../../../environments/environment';
 
 
-export interface State {
+export interface AppState {
 
+  [fromUserLogged.userLoggedFeatureKey]: fromUserLogged.UserLoggedState;
 }
 
-export const reducers: ActionReducerMap<State> = {
+export const reducers: ActionReducerMap<AppState> = {
 
+  [fromUserLogged.userLoggedFeatureKey]: fromUserLogged.reducer
 };
 
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
