37c714ba869ca8beafb3edbe10d535801f37f990
AKvin <kevin.alvespinto@gmail.com>
2020-09-10 10:04:31 +0200

    add user state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,15 @@ import { Component, OnInit, VERSION } from '@angular/core';
 import { UtilsService } from './shared/services/utils/utils.service';
 import { AppConstants } from './shared/constants/app.constants';
 import { Router, NavigationEnd } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { User } from './shared/models/user';
+import { Store, select } from '@ngrx/store';
+import { AppState } from './state/reducers';
+import { getUserLogged } from './state/selectors/user.selectors';
+import { refreshUser } from './state/actions/user.actions';
+import { AuthService } from './shared/services/auth.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,11 +26,16 @@ export class AppComponent implements OnInit {
   showHead = false;
   showNavPartenaire = false;
 
-  constructor(private utilsService: UtilsService, private router: Router) {
+  user$: Observable<User>;
+
+  constructor(private utilsService: UtilsService
+            , private router: Router
+            , private store: Store<AppState>
+            , private authService: AuthService) {
     // on route change to '/login', set the variable showHead to false
     router.events.forEach((event) => {
       if (event instanceof NavigationEnd) {
-        if (event.url === '/login') {
+        if (event.url === '/connexion/signin' || event.url === '/connexion/signup') {
           this.showHead = false;
           this.showNavPartenaire = false;
         } else {
@@ -30,10 +44,8 @@ export class AppComponent implements OnInit {
         }
 
         if (event.url === '/profil'){
-          this.showHead = true;
           this.showNavPartenaire = false;
         } else {
-          this.showHead = true;
           this.showNavPartenaire = true;
         }
       }
@@ -43,6 +55,20 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.utilsService.initFaIcons();
+    if (this.authService.isLoggedIn()){
+      console.log('refreshed');
+      this.refreshUserSession();
+    }
+    this.getUserLogged();
+
+  }
+
+  getUserLogged() {
+    this.user$ = this.store.pipe(select(getUserLogged));
+  }
+
+  refreshUserSession(){
+    this.store.dispatch(refreshUser());
   }
 
 }
