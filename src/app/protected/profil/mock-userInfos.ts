00a4e8b5792cf1d20b5d49e67d99685eeb992bae
Bryce Tavares <bryce.tavares@gmail.com>
2020-09-15 21:40:30 +0200

    template profil info perso + mock user info perso

---
+++ src/app/protected/profil/mock-userInfos.ts
@@ -0,0 +1,9 @@
+import { UserInfo } from './userInfo';
+
+export const USERINFOS: UserInfo[] = [
+  {id: 1, lastName: 'Lafondu', firstName: 'Roger', gender: 'm', birthdate: '1975-05-14', address: '2 rue de la Boustifaille'},
+  {id: 2, lastName: 'Moitessier', firstName: 'Philippe', gender: 'm', birthdate: '1980-06-20', address: '14 rue de la Bastille'},
+  {id: 3, lastName: 'Duclos', firstName: 'Aurélien', gender: 'm', birthdate: '1994-01-25', address: '15 avenue des champs'},
+  {id: 4, lastName: 'Lagarde', firstName: 'Sophie',  gender: 'f', birthdate: '1993-05-13', address: '20 rue de Lavoisier'},
+  {id: 5, lastName: 'Lavaud', firstName: 'Marion', gender: 'f', birthdate: '1997-10-15', address: '27 avenue de Mozart'}
+];
---
+++ src/app/protected/profil/userInfo.ts
@@ -0,0 +1,8 @@
+export interface UserInfo {
+  id: number;
+  lastName: string;
+  firstName: string;
+  gender: string;
+  birthdate: string;
+  address: string;
+}
